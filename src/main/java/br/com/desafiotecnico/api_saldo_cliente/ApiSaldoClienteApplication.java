a0a4e8cd8c1d2942fbda482e4558a739f1f8eb79
wellbenicio <wellynton.benicio@gmail.com>
2026-03-15 00:33:06 -0300

    base projeto

---
+++ src/main/java/br/com/desafiotecnico/api_saldo_cliente/ApiSaldoClienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafiotecnico.api_saldo_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSaldoClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSaldoClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafiotecnico/api_saldo_cliente/ApiSaldoClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiotecnico.api_saldo_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSaldoClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
