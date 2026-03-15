494bc9c783f2b912410ba9a3ac27b0d53082da83
Wellynton Benicio <62622936+wellbenicio@users.noreply.github.com>
2026-03-15 16:43:00 -0300

    Padroniza comando da aplicação e DTO HTTP de consulta de saldo

--- src/main/java/br/com/desafiotecnico/api_saldo_cliente/aplicacao/servico/ServicoConsultaSaldoConta.java
+++ src/main/java/br/com/desafiotecnico/api_saldo_cliente/aplicacao/servico/ServicoConsultaSaldoConta.java
@@ -1,6 +1,5 @@
 package br.com.desafiotecnico.api_saldo_cliente.aplicacao.servico;
 
-import br.com.desafiotecnico.api_saldo_cliente.aplicacao.porta.entrada.ConsultarSaldoContaComando;
 import br.com.desafiotecnico.api_saldo_cliente.aplicacao.porta.entrada.ConsultarSaldoContaPortaEntrada;
 import br.com.desafiotecnico.api_saldo_cliente.aplicacao.porta.entrada.comando.ConsultarSaldoContaComando;
 import br.com.desafiotecnico.api_saldo_cliente.aplicacao.porta.saida.RepositorioSaldoContaPortaSaida;
@@ -18,11 +17,6 @@ public class ServicoConsultaSaldoConta implements ConsultarSaldoContaPortaEntrad
         this.repositorioSaldoContaPortaSaida = repositorioSaldoContaPortaSaida;
     }
 
-    /**
-     * Regra de autorização por titularidade mantida no caso de uso.
-     * A autenticação (quem é o usuário) será tratada pela camada de segurança
-     * quando integrada nas próximas etapas.
-     */
     @Override
     public SaldoConta consultar(ConsultarSaldoContaComando comando) {
         SaldoConta saldoConta = repositorioSaldoContaPortaSaida
